813ce2392271cf21897b0f36b230eaf895d98d9d
shenghanqin <shenghanqin@hujiang.com>
2020-07-30 12:38:49 +0800

    update 基础文件

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@xiaoxili/react-ui-mode-cc'
+import '@xiaoxili/react-ui-mode-cc/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
