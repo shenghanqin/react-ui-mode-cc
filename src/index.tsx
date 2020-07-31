5544fef3ecb80cb910e1495d7b2390e23fa26f3c
shenghanqin <shenghanqin@163.com>
2020-07-31 22:14:20 +0800

    update 更新新的写法

--- src/index.tsx
+++ src/index.tsx
@@ -10,45 +10,34 @@ import './constants.css';
 //   return resultStr === 'micromessenger'
 // }
 
-const mqlMedia = window.matchMedia('(orientation: portrait)')
+// const mqlMedia = window.matchMedia('(orientation: portrait)')
 // const isPadWeixin = isWeixin() && userAgentLowerCase.includes('ipad')
 
-function onMatchMediaChange(mql:any = mqlMedia) {
-  if (mql.matches) {
-    //竖屏
-    // console.log('此时竖屏')
-    return 'portrait'
-  } else {
-    //横屏
-    // console.log('此时横屏')
-    return 'horizontal'
-  }
+// function onMatchMediaChange(mql:any = mqlMedia) {
+//   if (mql.matches) {
+//     //竖屏
+//     // console.log('此时竖屏')
+//     return 'portrait'
+//   } else {
+//     //横屏
+//     // console.log('此时横屏')
+//     return 'horizontal'
+//   }
 
-}
+// }
 
 // 输出当前屏幕模式
-const getUiMode = (uiMode = '', mql: MediaQueryList | MediaQueryListEvent) => {
-  if (uiMode) return uiMode
+const getUiMode = (widthMediaString: string) => {
   
-  // if (isPadWeixin) return 'mobile'
-
-  if (!('onorientationchange' in window)) return 'pc'
-
-  // if (!('onchange' in mqlMedia)) return 'mobile'
-  console.log('mql :>> ', mql);
-
-  let status = onMatchMediaChange(mql)
-  console.log('status :>> ', status);
-  let width = status === 'portrait' ? Math.min(window.innerWidth, window.innerHeight) : Math.max(window.innerWidth, window.innerHeight)
-
-  if (width > 1040) return 'pc'
-
-  return 'mobile'
+  let widthMediaQuery = window.matchMedia(widthMediaString)
+  console.log('widthMediaString :>> ', widthMediaString, widthMediaQuery);
+  return !widthMediaQuery.matches ? 'pc' : 'mobile'
 
 }
 
 const getIsPcMode = (uiMode: string) => uiMode === 'pc'
 
+const getWidthMediaString = (widthMedia: number) => `(max-width: ${widthMedia}px)`
 
 /**
  * rem适配, 适用于移动端适配
@@ -61,6 +50,12 @@ interface UIProps {
 }
 
 interface UIState {
+  /**
+   * 单一模式，要么是pc，要么是Mobile
+   */
+  isSingleMode: boolean
+  widthMedia: number,
+  widthMediaString: string,
   uiMode: string
   isPCMode: boolean
 }
@@ -72,38 +67,70 @@ export function withUiMode(Cmp: React.ComponentType, options: OptionsProps) {
   return class WithUIRem extends React.Component<UIProps, UIState> {
     constructor(props: UIProps) {
       super(props)
-      let uiMode = getUiMode('', mqlMedia)
-      let isPCMode = getIsPcMode(uiMode)
-      const { widthMedia } = options
-      console.log(1)
-      console.log('mqlMedia', mqlMedia, 'onchange' in mqlMedia, mqlMedia.onchange)
-      console.log('widthMedia :>> ', widthMedia);
-      console.log('2 :>> ', 2);
+      // 恒定 pc
+      // 恒定 mobile
+      // 需要转换的
+      
+      
+      const { widthMedia = 1023 } = options
+      let widthMediaString = getWidthMediaString(widthMedia)
+      let widthMediaQuery = window.matchMedia(widthMediaString)
+      console.log('this.widthMediaString :>> ', widthMediaString)
+      console.log(1, widthMediaQuery)
+      
+      let uiMode = 'mobile'
+      let isPCMode = false
+      let isSingleMode = false
+
+      if (!('onorientationchange' in window)) {
+        isSingleMode = true
+        uiMode = 'pc'
+        isPCMode = true
+      } else if (Math.max(window.innerWidth, window.innerHeight) < widthMedia) {
+        isSingleMode = true
+      } else {
+        uiMode = getUiMode(widthMediaString)
+        // uiMode = getUiMode(mqlMedia)
+        isPCMode = getIsPcMode(uiMode)
+        // console.log('mqlMedia', mqlMedia, 'onchange' in mqlMedia, mqlMedia.onchange)
+        // console.log('widthMedia :>> ', widthMedia);
+
+        // console.log('2 :>> ', 2);
+
+
+      }
+
+      console.log('isSingleMode :>> ', isSingleMode);
+
+
       this.state = {
+        isSingleMode,
+        widthMedia,
+        widthMediaString,
         uiMode: uiMode,
         isPCMode: isPCMode,
       }
     }
 
     componentDidMount() {
-      if(('onchange' in mqlMedia)) {
-        mqlMedia.addListener(this.changeUiMode)
-      } else {
+      // if(('onchange' in mqlMedia)) {
+      //   mqlMedia.addListener(this.changeUiMode)
+      // } else {
         window.addEventListener('orientationchange', this.onOrientationChange)
-      }
+      // }
     }
 
     componentWillUnmount() {
-      if ('onchange' in mqlMedia) {
-        mqlMedia.removeListener(this.changeUiMode)
-      } else {
+      // if ('onchange' in mqlMedia) {
+      //   mqlMedia.removeListener(this.changeUiMode)
+      // } else {
         window.removeEventListener('orientationchange', this.onOrientationChange)
-      }
+      // }
     }
 
     changeUiMode = (event: MediaQueryListEvent) => {
       console.log('"change" :>> ', event);
-      let newUiMode = getUiMode('', event)
+      let newUiMode = getUiMode(this.state.widthMediaString)
       if (newUiMode !== this.state.uiMode) {
         this.setState({
           isPCMode: getIsPcMode(newUiMode),
@@ -116,9 +143,9 @@ export function withUiMode(Cmp: React.ComponentType, options: OptionsProps) {
 
       console.log('orientation :>> ', window.orientation);
       setTimeout(() => {
-        let mqlNow = window.matchMedia('(orientation: portrait)')
-        console.log('mqlNow :>> ', mqlNow);
-        let newUiMode = getUiMode('', mqlNow)
+        // let mqlNow = window.matchMedia('(orientation: portrait)')
+        // console.log('mqlNow :>> ', mqlNow);
+        let newUiMode = getUiMode(this.state.widthMediaString)
         console.log('newUiMode :>> ', newUiMode);
         if (newUiMode !== this.state.uiMode) {
           this.setState({
